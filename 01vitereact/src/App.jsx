443ac04525121a64d2159320d72cb83d914581ef
Ekam <ekambhelle030@gmail.com>
2025-09-14 03:28:19 +0530

    React custom create react element

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,10 @@
 import Chai from './chai.jsx'
 function App() {
+  const username = 'Ekam'
   return (
     <>
-    <h1>Hello react + vite with chai</h1>
+    <h1>Hello react + vite with {username}</h1>
+    {/* {} will contain - evaluated expression (final expression)*/}
     <Chai/>
     </>
   )
